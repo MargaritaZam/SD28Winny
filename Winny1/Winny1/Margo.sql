1d20f203e6db60f2a6c5c93cd06986644a4e68e8
margarita.zamoshch <margarita.zamoshch@robertsoncollege.net>
2018-06-07 12:27:09 -0500

    new folders

--- Winny1/Winny1/Margo.sql
+++ Winny1/Winny1/Margo.sql
@@ -13,14 +13,17 @@ locationName varchar(60)
 )
 go
 insert into tbLocation(locationNAme)values
-("Anywhere"), ("North"), ("Northeast"), ("East"), ("Southeast"), ("South"), ("Southwest"),
-("West"), ("Northwest"), ("Downtown"), ("Airport/West"), ("Just Outside Winnipeg")
+('North'),('Northeast'),('East'),('Southeast'),
+('South'),('Southwest'),('West'),('Northwest'),
+('Downtown'),('Airport/West'),('Just Outside Winnipeg')
 go
 
+select*from tbLocation
+go
 create table tbAttractions(
 attractionID int identity(1,1) primary key,
 attractionCategory varchar(150),
-atName varhar(100),
+atName varchar(100),
 atDesc varchar(650),
 atAddress varchar(100),
 atPhone varchar(20),
@@ -30,3 +33,113 @@ location int foreign key references tbLocation(locationID)
 
 )
 go
+
+select*from tbAttractions
+go
+
+create procedure spAttractions(
+@id int =null,
+@category varchar(150)=null,
+@name varchar(100)=null,
+@desc varchar(650)=null,
+@address varchar(100)=null,
+@phone varchar(20)=null,
+@website varchar(200)=null,
+@image varchar(60)=null,
+@location varchar(60)=null,
+@crud varchar(1)
+)
+as begin
+if @crud ='r'
+begin
+select attractionID, attractionCategory,
+atName, atDesc, atAddress, atPhone,
+atWebsite, './Pictures/'+ atImage, location from tbAttractions
+where attractionID=isnull(@id, attractionID)
+end
+
+else if @crud='c'
+begin
+insert into tbAttractions(attractionCategory, atName,
+atDesc, atAddress, atPhone,
+atWebsite, atImage, location)values
+(@category, @name,
+@desc, @address, @phone,
+@website, @image, @location)
+end
+else if @crud='d'
+begin
+delete from tbAttractions where attractionID=@id
+end
+else if @crud='u'
+begin
+update tbAttractions
+set  attractionCategory =@category,
+     atName =@name,
+     atDesc=@desc,
+     atAddress =@desc,
+     atPhone =@phone,
+     atWebsite =@website,
+     atImage =@image,
+     location =@location
+	 where attractionID=@id
+end
+end
+go
+
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
+exec spAttractions @crud='c', @category='',
+                              @name='',
+                              @desc='', 
+							  @address='', 
+							  @phone='',
+                              @website='', 
+							  @image='', 
+							  @location=''
