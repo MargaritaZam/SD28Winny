a656561633ae29a7e2783f5fdf20adcb56483a0c
anjali.patel <anjali.patel@robertsoncollege.net>
2018-06-11 13:43:20 -0700

    Restaurant.aspx

--- Winny1/Winny1/Restaurants.aspx.cs
+++ Winny1/Winny1/Restaurants.aspx.cs
@@ -12,23 +12,67 @@ namespace Winny1
 {
     public partial class Restaurants : System.Web.UI.Page
     {
+        PagedDataSource adsource;
+        int pos;
         string conn = "Data Source= localhost; Initial Catalog=dbRestaurants; Integrated Security= SSPI";
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                this.ViewState["vs"] = 0;
+            }
+            pos = (int)this.ViewState["vs"];
             loadRestaurants();
 
         }
         public void loadRestaurants()
         {
+            
             DAL myDal = new DAL(conn);
-            myDal.AddParam("@crud", "a");
+            adsource = new PagedDataSource();
+            myDal.AddParam("@crud","a");
             DataSet ds = myDal.ExecuteProcedure("spRestaurants");
-            
-            dlRestaurant.DataSource = ds.Tables[0];
+            adsource.DataSource = ds.Tables[0].DefaultView;
+            adsource.PageSize = 3;
+            adsource.AllowPaging = true;
+            adsource.CurrentPageIndex = pos;
+            btnfirst.Enabled = !adsource.IsFirstPage;
+            btnprevious.Enabled = !adsource.IsFirstPage;
+            btnlast.Enabled = !adsource.IsLastPage;
+            btnnext.Enabled = !adsource.IsLastPage;
+            dlRestaurant.DataSource = adsource;
             dlRestaurant.DataBind();
-            
-            
 
+
+
+        }
+
+        protected void btnfirst_Click(object sender, EventArgs e)
+        {
+            pos = 0;
+            loadRestaurants();
+        }
+
+        protected void btnprevious_Click(object sender, EventArgs e)
+        {
+            pos = (int)this.ViewState["vs"];
+            pos -= 1;
+            this.ViewState["vs"] = pos;
+            loadRestaurants();
+        }
+
+        protected void btnnext_Click(object sender, EventArgs e)
+        {
+            pos = (int)this.ViewState["vs"];
+            pos += 1;
+            this.ViewState["vs"] = pos;
+            loadRestaurants();
+        }
+
+        protected void btnlast_Click(object sender, EventArgs e)
+        {
+            pos = adsource.PageCount - 1;
+            loadRestaurants();
         }
     }
 }
