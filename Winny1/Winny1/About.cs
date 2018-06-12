7b1cb10c74d04ec4a277b5355dafb842360287dd
Tracy59 <tracy.mccormack@RobertsonCollege.net>
2018-06-12 15:02:27 -0500

    Created About.cs

---
+++ Winny1/Winny1/About.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Winny1
+{
+    public class About
+    {
+        public int AboutID { get; set; }
+        public string AboutTitle { get; set; }
+        public string AboutDescription { get; set; }
+    }
+}
