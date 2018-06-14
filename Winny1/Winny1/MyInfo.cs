7cff81ff73b1b0d0f57dc9554a5acbe97a1a57d1
margarita.zamoshch <margarita.zamoshch@robertsoncollege.net>
2018-06-14 15:59:05 -0500

    11

---
+++ Winny1/Winny1/MyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Winny1
+{
+    public class MyInfo
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public string image { get; set; }
+        public string website { get; set; }
+
+        public MyInfo(int id)
+        {
+
+        }
+    }
+}
---
+++ Winny1/Winny1/info.aspx.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace Winny1
+{
+    public partial class info : System.Web.UI.Page
+    {
+        SqlConnection conn = new SqlConnection("Data Source = localhost; Initial Catalog = dbGroupProject; Integrated Security = SSPI");
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetInfo();
+            }
+        }
+        private void GetInfo()
+        {
+            string id = Request.QueryString["id"].ToString();
+            MyInfo inf = new MyInfo(Convert.ToInt32(id));
+            Image.ImageUrl = inf.image;
+            txtName.Text = inf.name;
+            txtDesc.Text = inf.description;
+            txtPhone.Text = inf.phone;
+            txtAddress.Text = inf.address;
+            hlWeb.Text = inf.website;
+        }
+    }
+}
