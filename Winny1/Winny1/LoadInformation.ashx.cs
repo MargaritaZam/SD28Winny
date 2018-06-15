c727b05234aa4212d403e29a0ac6d0edea0a9bcb
Tracy59 <tracy.mccormack@RobertsonCollege.net>
2018-06-15 14:51:29 -0500

    Added Hotels to LoadInformation

--- Winny1/Winny1/LoadInformation.ashx.cs
+++ Winny1/Winny1/LoadInformation.ashx.cs
@@ -15,6 +15,8 @@ namespace Winny1
         string conn = "Data Source= localhost; Initial Catalog=dbRestaurants; Integrated Security= SSPI";
         string conn1 = "Data Source=localhost; Initial Catalog=dbShopping; Integrated Security=SSPI";
         string conn2 = "Data Source= localhost; Initial Catalog=dbGroupProject; Integrated Security= SSPI";
+        string conn3 = "Data Source=localhost;Initial Catalog=dbOmegaProject;Integrated Security=SSPI";
+
         public void ProcessRequest(HttpContext context)
         {
             string id = context.Request.Form["Id"];
@@ -96,9 +98,30 @@ namespace Winny1
 
 
             }
+            else if (choice == "Hotel")
+            {
+                DAL myDal = new DAL(conn3);
+                myDal.AddParam("@crud", "r");
+                myDal.AddParam("@id", id);
+                DataSet ds = myDal.ExecuteProcedure("spHotelsCrud");
 
-
-
+                for (int i = 0; i < ds.Tables[0].Rows.Count; i++)
+                {
+                    result = result +/* "{\"Id\": \"" + ds.Tables[0].Rows[i]["HotelId"].ToString() +*/
+                                     /* "\",*/ "{\"Name\":\"" + ds.Tables[0].Rows[i]["HotelName"].ToString() +
+                                         "\", \"Price\":\"" + ds.Tables[0].Rows[i]["HotelPrice"].ToString() +
+                                         "\", \"Stars\":\"" + ds.Tables[0].Rows[i]["HotelStars"].ToString() +
+                                         "\", \"Description\":\"" + ds.Tables[0].Rows[i]["HotelDescription"].ToString() +
+                                         "\", \"ContactNo\":\"" + ds.Tables[0].Rows[i]["HotelPhoneNumber"].ToString() +
+                                         "\", \"Address\":\"" + ds.Tables[0].Rows[i]["HotelAddress"].ToString() +
+                                         "\", \"PostalCode\":\"" + ds.Tables[0].Rows[i]["HotelPostalCode"].ToString() +
+                                         "\", \"Website\":\"" + ds.Tables[0].Rows[i]["HotelWebsite"].ToString() +
+                                         "\", \"path\":\"" + ds.Tables[0].Rows[i]["Hotel_path"].ToString() +
+                                         "\", \"Location\":\"" + ds.Tables[0].Rows[i]["HotelLocationID"].ToString() +
+                                         "\"}, ";
+                }
+                
+            }
 
             result = result.Substring(0, result.Length - 2) + " ]}";
             return result;
