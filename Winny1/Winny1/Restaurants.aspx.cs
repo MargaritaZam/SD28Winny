715ee1195112387e33494e3b8f965c46fa4e4447
natalia.shmer <natalia.shmer@robertsoncollege.net>
2018-06-29 10:38:36 -0500

    chatbot

--- Winny1/Winny1/Restaurants.aspx.cs
+++ Winny1/Winny1/Restaurants.aspx.cs
@@ -57,10 +57,10 @@ namespace Winny1
         public void loadRestaurants()
         {
             adsource = new PagedDataSource();
-            adsource.DataSource =rs.LoadRestaurant();
+            adsource.DataSource = rs.LoadRestaurant();
             adsource.AllowPaging = true;
             adsource.PageSize = 3;
-            
+
             adsource.CurrentPageIndex = pos;
             btnfirst.Enabled = !adsource.IsFirstPage;
             btnprevious.Enabled = !adsource.IsFirstPage;
