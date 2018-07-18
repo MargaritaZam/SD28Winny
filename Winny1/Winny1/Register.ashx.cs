6479ebdba1db970bf423e4b086709974156452ec
anjali.patel <anjali.patel@robertsoncollege.net>
2018-07-18 12:32:22 -0700

    fsfdsf

---
+++ Winny1/Winny1/Register.ashx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Winny1
+{
+    /// <summary>
+    /// Summary description for Register
+    /// </summary>
+    public class Register : IHttpHandler
+    {
+        SqlConnection conn = new SqlConnection(
+          "Data Source=localhost;Initial Catalog=dbGroupProject; Integrated Security=SSPI");
+        DataSet ds = new DataSet();
+        public void ProcessRequest(HttpContext context)
+        {
+            context.Response.ContentType = "text/plain";
+            context.Response.ContentType = "text/plain";
+
+            string first = context.Request.Form["FirstName"];
+            string last = context.Request.Form["LastName"];
+            string Address = context.Request.Form["Address"];
+            string Phone = context.Request.Form["PhoneNo"];
+            string email = context.Request.Form["email"];
+            string pass = context.Request.Form["pass"];
+
+            SqlCommand cmd = new SqlCommand("spUser", conn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@crud", "c");
+            cmd.Parameters.AddWithValue("@firstName", first);
+            cmd.Parameters.AddWithValue("@lastName", last);
+            cmd.Parameters.AddWithValue("@phoneNumber", Phone);
+            cmd.Parameters.AddWithValue("@Address", Address);
+            cmd.Parameters.AddWithValue("@Email", email);
+            cmd.Parameters.AddWithValue("@Password", pass);
+            cmd.Parameters.AddWithValue("@AccessLevel", "c");
+
+            conn.Open();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Winny1/Winny1/login.ashx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Winny1
+{
+    /// <summary>
+    /// Summary description for login
+    /// </summary>
+    public class login : IHttpHandler
+    {
+        string Access;
+        public void ProcessRequest(HttpContext context)
+        {
+            string user = context.Request.Form["email"];
+            string pass = context.Request.Form["password"];
+            DataSet ds = Login(user, pass);
+            context.Response.ContentType = "text/javascript";
+            context.Response.Write(Access);
+        }
+        private DataSet Login(string user, string pass)
+        {
+
+            SqlConnection conn = new SqlConnection(
+           "Data Source=localhost;Initial Catalog=dbGroupProject; Integrated Security=SSPI");
+            DataSet ds = new DataSet();
+            SqlDataAdapter da = new SqlDataAdapter("spLogin", conn);
+            da.SelectCommand.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand.Parameters.AddWithValue("@email", user);
+            da.SelectCommand.Parameters.AddWithValue("@password", pass);
+            conn.Open();
+            da.Fill(ds);
+            conn.Close();
+
+            Access = ds.Tables[0].Rows[0]["accessLevel"].ToString();
+            return ds;
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
