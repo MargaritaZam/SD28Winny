5a0679f8e0c3b5a6ae9cfe2eb649de0e12732c19
anjali.patel <anjali.patel@robertsoncollege.net>
2018-07-20 10:28:25 -0700

    dgg

--- Winny1/Winny1/Chat.aspx.cs
+++ Winny1/Winny1/Chat.aspx.cs
@@ -15,6 +15,7 @@ namespace Winny1
     {
         public string UserName = "admin";
         public string UserImage = "/images/DP/dummy.png";
+        //jdhdj
         ConnClass ConnC = new ConnClass();
         protected void Page_Load(object sender, EventArgs e)
         {
